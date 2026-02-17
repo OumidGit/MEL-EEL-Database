46cea8a4f1ed7adb822d17063709de9a7fc332c9
OumidGit <AOumid1002@gmail.com>
2026-02-17 10:05:40 -0500

    Update 04-create-core-staging.sql

--- 04-create-core-staging.sql
+++ 04-create-core-staging.sql
@@ -33,21 +33,59 @@ CREATE TABLE audit.etl_row_errors(
 );
 
 -- Staging (parsed rows)
-CREATE TABLE stg.equipment_mech(
-    project_code      NVARCHAR(50) NOT NULL,
-    file_id           BIGINT NULL,
-    equipment_tag     NVARCHAR(100) NULL,
-    description       NVARCHAR(400) NULL,
-    eq_type           NVARCHAR(100) NULL,
-    connected_load_kw DECIMAL(18,4) NULL,
-    voltage_v         DECIMAL(18,4) NULL,
-    power_factor_pf   DECIMAL(9,6) NULL,
-    starter_type      NVARCHAR(100) NULL,
-    location          NVARCHAR(200) NULL,
-    raw_row_json      NVARCHAR(MAX) NULL
+CREATE TABLE stg.MEL(
+    -- Mechanical Section
+    project_code           NVARCHAR(50) NOT NULL,
+    file_id                BIGINT NULL,
+    Area                   NVARCHAR(100) NULL,
+    eq_type                NVARCHAR(100) NULL,
+    code                   NVARCHAR(100) NULL,
+    equipment_tag          NVARCHAR(100) NULL,
+    description            NVARCHAR(400) NULL,
+    rev                    NVARCHAR(400) NULL,
+    qty                    NVARCHAR(400) NULL,
+    nameplate_power        DECIMAL(18,4) NULL,
+    nameplate_power_unit   NVARCHAR(400) NULL,
+    utilization_factor     DECIMAL(18,4) NULL,
+    standby                NVARCHAR(400) NULL,
+    starter_type           NVARCHAR(400) NULL,
+    load_type              NVARCHAR(400) NULL,
+    duty                   NVARCHAR(400) NULL,
+    emergency_load         NVARCHAR(400) NULL,
+    poles                  NVARCHAR(400) NULL,
+    -- Electrical Section
+    forced_duty            NVARCHAR(100) NULL,
+    duty_type              NVARCHAR(100) NULL,
+    demand_factor          DECIMAL(18,4) NULL,
+    power_factor           DECIMAL(9,6) NULL,
+    efficiency             DECIMAL(9,6) NULL,
+    voltage_v              DECIMAL(18,4) NULL,
+    phase                  DECIMAL(18,4) NULL
+    eroom                  NVARCHAR(100) NULL,
+    transformer            NVARCHAR(100) NULL,
+    mcc_switchgear         NVARCHAR(100) NULL,
+    forced_nameplate_power DECIMAL(18,4) NULL,
+    bucket_size            NVARCHAR(100) NULL,
+    forced_starter_type    NVARCHAR(100) NULL,
+    installed_kw           DECIMAL(18,4) NULL,
+    installed_kvar         DECIMAL(18,4) NULL,
+    installed_kva          DECIMAL(18,4) NULL,
+    peak_kw                DECIMAL(18,4) NULL,
+    peak_kvar              DECIMAL(18,4) NULL,
+    peak_kva               DECIMAL(18,4) NULL,
+    average_kw             DECIMAL(18,4) NULL,
+    average_kvar           DECIMAL(18,4) NULL,
+    average_kva            DECIMAL(18,4) NULL,
+    annual_load_mwh        DECIMAL(18,4) NULL,
+    fla                    DECIMAL(18,4) NULL,
+    current_draw           DECIMAL(18,4) NULL,
+    fla_125%               DECIMAL(18,4) NULL,
+    -- Misc 
+    location               NVARCHAR(200) NULL,
+    raw_row_json           NVARCHAR(MAX) NULL
 );
 
-CREATE TABLE stg.equipment_elec(
+CREATE TABLE stg.EEL(
     project_code  NVARCHAR(50) NOT NULL,
     file_id       BIGINT NULL,
     equipment_tag NVARCHAR(100) NULL,
@@ -60,7 +98,7 @@ CREATE TABLE stg.equipment_elec(
     raw_row_json  NVARCHAR(MAX) NULL
 );
 
-CREATE TABLE core.equipment_mech(
+CREATE TABLE core.MEL(
     project_code      NVARCHAR(50) NOT NULL,
     equipment_tag     NVARCHAR(100) NOT NULL,
     description       NVARCHAR(400) NULL,
@@ -75,7 +113,7 @@ CREATE TABLE core.equipment_mech(
     CONSTRAINT PK_core_mech PRIMARY KEY(project_code, equipment_tag)
 );
 
-CREATE TABLE core.equipment_elec(
+CREATE TABLE core.EEL(
     project_code      NVARCHAR(50) NOT NULL,
     equipment_tag     NVARCHAR(100) NOT NULL,
     cable_tag         NVARCHAR(100) NULL,
@@ -87,4 +125,5 @@ CREATE TABLE core.equipment_elec(
     last_modified_utc DATETIME2(3) NOT NULL DEFAULT SYSUTCDATETIME(),
     rowversion        ROWVERSION,
     CONSTRAINT PK_core_elec PRIMARY KEY(project_code, equipment_tag)
+
 );
