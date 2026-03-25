8180c32446557594f10dc7d1371e07c71f39f00c
OumidGit <AOumid1002@gmail.com>
2026-03-25 09:59:39 -0400

    Update create-cdc.sql

--- Tables/create-cdc.sql
+++ Tables/create-cdc.sql
@@ -8,14 +8,3 @@ BEGIN
     EXEC sys.sp_cdc_enable_db;
 END
 GO
-
--- Enable CDC on database (only once)
-IF NOT EXISTS (
-    SELECT 1 FROM sys.databases
-    WHERE name = 'MEL_EEL_Automation_Dev'
-    AND is_cdc_enabled = 1
-)
-BEGIN
-    EXEC sys.sp_cdc_enable_db;
-END
-GO
